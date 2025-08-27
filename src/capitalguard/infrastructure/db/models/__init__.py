d0b01e1a069bbbd48910b3c78d7a04ca29afc4a4
eng540 <132091714+eng540@users.noreply.github.com>
2025-08-27 23:16:28 +0300

    Update __init__.py

--- src/capitalguard/infrastructure/db/models/__init__.py
+++ src/capitalguard/infrastructure/db/models/__init__.py
@@ -1,1 +1,5 @@
-#all
+#--- START OF FILE: src/capitalguard/infrastructure/db/models/__init__.py ---
+from .base import Base
+from .recommendation import RecommendationORM
+from .auth import User, Role, UserRole
+#--- END OF FILE ---
