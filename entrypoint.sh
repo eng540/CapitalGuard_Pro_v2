0c22997416a1da6085c772826f740c71d02cacfc
eng540 <132091714+eng540@users.noreply.github.com>
2025-09-04 20:59:48 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
-# This script ensures the database is up-to-date before starting the main application.
 set -e
 
-echo "API Entrypoint: Running database migrations..."
+echo "Entrypoint: Running database migrations..."
 alembic upgrade head
 
-echo "API Entrypoint: Migrations complete. Starting application..."
-# The 'exec' command replaces the shell process with the command that follows.
-# '$@' evaluates to all the arguments passed to the script, which in this case
-# will be the CMD from the Dockerfile.
+echo "Entrypoint: Migrations complete. Starting supervisor..."
 exec "$@"
