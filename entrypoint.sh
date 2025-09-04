0b10086162382351337869361edd30f6b830742b
eng540 <132091714+eng540@users.noreply.github.com>
2025-09-04 12:46:09 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,11 @@
-# --- START OF FILE: entrypoint.sh ---
+#- START OF FILE: entrypoint.sh ---
 #!/bin/sh
-
-# Fail fast: Exit immediately if a command exits with a non-zero status.
 set -e
 
 echo "Running database migrations..."
 alembic upgrade head
 
 echo "Starting application..."
-# Important: Using 'sh -c' allows the expansion of variables like ${PORT}
-# that are passed from the Dockerfile's shell-form CMD.
+# نحتاج توسعة ${PORT} من CMD بصيغة shell-form
 exec sh -c "$@"
-# --- END OF FILE ---
+#--- END OF FILE: entrypoint.sh ---
