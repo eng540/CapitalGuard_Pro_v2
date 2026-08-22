693ad140f7854dc7cbf2aec43809bd03cb75dfa2
eng540 <1.32091714e+08+eng540@users.noreply.github.com>
2026-08-22 18:22:13 +0000

    feat: add safe user trade breakeven contract

--- src/capitalguard/application/services/trade_service.py
+++ src/capitalguard/application/services/trade_service.py
@@ -561,6 +561,7 @@ class TradeService:
     
     async def close_user_trade_async(self, *args, **kwargs): return await self.lifecycle_service.close_user_trade_async(*args, **kwargs)
     async def partial_close_user_trade_async(self, *args, **kwargs): return await self.lifecycle_service.partial_close_user_trade_async(*args, **kwargs)
+    async def move_user_trade_stop_to_breakeven_async(self, *args, **kwargs): return await self.lifecycle_service.move_user_trade_stop_to_breakeven_async(*args, **kwargs)
     async def close_recommendation_async(self, *args, **kwargs): return await self.lifecycle_service.close_recommendation_async(*args, **kwargs)
     async def partial_close_async(self, *args, **kwargs): return await self.lifecycle_service.partial_close_async(*args, **kwargs)
     async def update_sl_for_user_async(self, *args, **kwargs): return await self.lifecycle_service.update_sl_for_user_async(*args, **kwargs)
